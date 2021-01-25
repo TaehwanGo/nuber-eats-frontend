f225537b3b40b95b0fca122842a43a65b392befa
TaehwanGo <gth1123@naver.com>
2021-01-25 21:23:53 +0900

    #15.1 react hook form

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,40 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export const LoggedOutRouter = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const onChange = (event: any) => {
-    const {
-      target: { name, value },
-    } = event;
-    if (name === 'email') {
-      setEmail(value);
-    }
-    if (name === 'password') {
-      setPassword(value);
-    }
+  const { register, watch, handleSubmit, errors } = useForm();
+  //   console.log(watch()); // input에 타이핑 되는 것을 watch // onChange, event 같은 것을 대체
+  const onSubmit = () => {
+    // handleSubmit은 내 handleSubmit함수와 함께 불러와야 함
+    console.log(watch('email'));
   };
-
+  // handleSubmit은 onValid 와 onInvalid 둘다 줄 수 있음
+  const onInvalid = () => {
+    console.log("can't create account");
+  };
+  console.log(errors);
   return (
     <div>
       <h1>Logged Out</h1>
-      <form>
-        <input
-          onChange={onChange}
-          name="email"
-          type="email"
-          required
-          placeholder="email"
-          value={email}
-        />
-        <input
-          onChange={onChange}
-          value={password}
-          name="password"
-          type="password"
-          required
-          placeholder="password"
-        />
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            ref={register({
+              required: 'This is required',
+              //   validate: (email: string) => email.includes('@gmail.com'),
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/, // regular expression 으로도 검사 가능
+            })} // register를 등록한 것만으로 react에 의해 컨트롤 되는 form이 생성됨
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            ref={register({
+              required: true,
+            })}
+            name="password"
+            type="password"
+            required
+            placeholder="password"
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Submit</button>
       </form>
     </div>
   );
