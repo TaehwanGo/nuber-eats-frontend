b9b4f84501b0e8b828e90ea8176a70fc3fbc3005
TaehwanGo <gth1123@naver.com>
2021-01-25 19:13:16 +0900

    #15.0 Local Only Field

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { gql, useQuery, useReactiveVar } from '@apollo/client';
 import React from 'react';
+import { isLoggedInVar } from './apollo';
+import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
 
+const IS_LOGGED_IN = gql`
+  query isLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
 function App() {
-  return <LoggedOutRouter />;
+  // useQuery() : react-hooks 의 한 종류, for graphql
+  // const {
+  //   data: { isLoggedIn },
+  // } = useQuery(IS_LOGGED_IN); // app은 언제나 user가 logged in 상태인지를 묻고 로그인 상태에 맞는 라우터를 보여줌
+  const isLoggedIn = useReactiveVar(isLoggedInVar); // hooks(useReactiveVar)를 사용해서 gql 쿼리를 직접 작성하지 않고 state를 확인
+
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,21 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+
+export const isLoggedInVar = makeVar(false);
 
 export const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql', // backend uri
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              // field의 값을 반환하는 함수
+              return isLoggedInVar(); // Boolean(localStorage.getItem('token')); // local storage에 token이 있을 때엔 우리가 logged in 되었다는 것을 알려줌
+            },
+          },
+        },
+      },
+    },
+  }),
 });
