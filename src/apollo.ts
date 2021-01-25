8e1f8fa4ff4acb21dfcdda981a85a75f96189cca
TaehwanGo <gth1123@naver.com>
2021-01-25 20:48:08 +0900

    #15.1 before react hook form

--- src/apollo.ts
+++ src/apollo.ts
@@ -9,6 +9,7 @@ export const client = new ApolloClient({
       Query: {
         fields: {
           isLoggedIn: {
+            // reactive variable을 사용하면 field name을 기억할 필요도 없네
             read() {
               // field의 값을 반환하는 함수
               return isLoggedInVar(); // Boolean(localStorage.getItem('token')); // local storage에 token이 있을 때엔 우리가 logged in 되었다는 것을 알려줌
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import { isLoggedInVar } from '../apollo';
+import React, { useState } from 'react';
 
 export const LoggedOutRouter = () => {
-  const onClick = () => {
-    isLoggedInVar(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onChange = (event: any) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    }
+    if (name === 'password') {
+      setPassword(value);
+    }
   };
+
   return (
     <div>
       <h1>Logged Out</h1>
-      <button onClick={onClick}>Click to login</button>
+      <form>
+        <input
+          onChange={onChange}
+          name="email"
+          type="email"
+          required
+          placeholder="email"
+          value={email}
+        />
+        <input
+          onChange={onChange}
+          value={password}
+          name="password"
+          type="password"
+          required
+          placeholder="password"
+        />
+      </form>
     </div>
   );
 };
