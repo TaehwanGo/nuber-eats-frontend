3d8231e6d6eb8f444eaaaf1b673ff41ae7b2a72d
TaehwanGo <gth1123@naver.com>
2021-01-25 21:45:22 +0900

    #15.2 react hook form + typescript

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+interface IForm {
+  // interface인 것을 이름만으로 알기 위해 I를 추가함
+  email: string;
+  password: string;
+}
+
 export const LoggedOutRouter = () => {
-  const { register, watch, handleSubmit, errors } = useForm();
+  const { register, watch, handleSubmit, errors } = useForm<IForm>();
   //   console.log(watch()); // input에 타이핑 되는 것을 watch // onChange, event 같은 것을 대체
   const onSubmit = () => {
     // handleSubmit은 내 handleSubmit함수와 함께 불러와야 함
@@ -12,7 +18,6 @@ export const LoggedOutRouter = () => {
   const onInvalid = () => {
     console.log("can't create account");
   };
-  console.log(errors);
   return (
     <div>
       <h1>Logged Out</h1>
@@ -28,6 +33,14 @@ export const LoggedOutRouter = () => {
             type="email"
             placeholder="email"
           />
+          {errors.email?.message && (
+            <span className="font-bold text-red-600">
+              {errors.email?.message}
+            </span>
+          )}
+          {errors.email?.type === 'pattern' && (
+            <span className="font-bold text-red-600">Only gmail allowed</span>
+          )}
         </div>
         <div>
           <input
