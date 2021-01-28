490b0c98537d828f6ce1b1282673a50540ef78ce
TaehwanGo <gth1123@naver.com>
2021-01-29 05:42:27 +0900

    #17.9 Restaurant detail part two

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -37,5 +37,24 @@ export const RestaurantDetail = () => {
     },
   });
   console.log(data);
-  return <h1>Restaurant</h1>;
+  return (
+    <div>
+      <div
+        className="bg-gray-300 bg-center bg-cover py-32 md:py-40 lg:py-52"
+        style={{
+          backgroundImage: `url(${data?.findRestaurantById.restaurant?.coverImage})`,
+        }}
+      >
+        <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto">
+          <div className="max-w-screen-sm py-4 text-white">
+            <h4 className="text-4xl mb-2">
+              {data?.findRestaurantById.restaurant?.name}
+            </h4>
+            <h5>{data?.findRestaurantById.restaurant?.category?.name}</h5>
+            <h5>{data?.findRestaurantById.restaurant?.address}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
