eb2762bbf44647f932bb772b2b0d8f3483b1966a
TaehwanGo <gth1123@naver.com>
2021-01-29 02:02:45 +0900

    #17.4 Search part one

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Restaurants } from '../pages/client/restaurant';
 import { Header } from '../components/header';
 import { useMe } from '../hooks/useMe';
 import { ConfirmEmail } from '../pages/user/confirm-email';
 import { EditProfile } from '../pages/user/edit-profile';
 import { NotFound } from '../pages/404';
+import { Search } from '../pages/client/search';
 
 //<></> : fragment : parent없이 많은 element를 동시에 return 할 수 있게 됨
 const ClientRoutes = [
-  <Route key={1} path="/confirm" exact>
+  <Route key={1} path="/confirm">
     <ConfirmEmail />
   </Route>,
   <Route key={2} path="/" exact>
     <Restaurants />
   </Route>,
-  <Route key={3} path="/edit-profile" exact>
+  <Route key={3} path="/edit-profile">
     <EditProfile />
+  </Route>,
+  <Route key={4} path="/search">
+    <Search />
   </Route>,
 ];
 
@@ -41,7 +40,6 @@ export const LoggedInRouter = () => {
       <Header />
       <Switch>
         {data.me.role === 'Client' && ClientRoutes}
-        {/* <Redirect to="/" /> */}
         <Route>
           <NotFound />
         </Route>
