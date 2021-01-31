c42de0cac14114984261e1b7881b3e6a04fa625e
TaehwanGo <gth1123@naver.com>
2021-02-01 04:16:30 +0900

    #18.10 Create Account Tests part three

--- src/pages/__test__/create-account.spec.tsx
+++ src/pages/__test__/create-account.spec.tsx
@@ -6,6 +6,19 @@ import { CreateAccount, CREATE_ACCOUNT_MUTATION } from '../create-account';
 import { render, waitFor, RenderResult } from '../../test-utils';
 import { UserRole } from '../../__generated__/globalTypes';
 
+// how to mock hooks
+const mockPush = jest.fn();
+jest.mock('react-router-dom', () => {
+  // jest가 module을 전부 cover하면서 beforeEach에서 쓰고 있는 Router까지 고장남
+  const realModule = jest.requireActual('react-router-dom'); // 실제 모듈을 필요로 함
+  return {
+    ...realModule, // 실제 모듈 기능 + mock이 필요한 부분만 mock
+    useHistory: () => {
+      return { push: mockPush };
+    },
+  };
+});
+
 describe('<CreateAccount />', () => {
   let mockedClient: MockApolloClient;
   let renderResult: RenderResult;
@@ -84,6 +97,10 @@ describe('<CreateAccount />', () => {
     });
     expect(window.alert).toHaveBeenCalledWith('Account Created! Log in now!');
     const mutationError = getByRole('alert');
+    expect(mockPush).toHaveBeenCalledWith('/');
     expect(mutationError).toHaveTextContent('mutation-error');
+  });
+  afterAll(() => {
+    jest.clearAllMocks(); // test 후 모든 것을 제자리로 돌려 놓음(hooks mock 이후)
   });
 });
