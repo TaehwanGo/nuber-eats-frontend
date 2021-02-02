a353a5184d5b76fff31e00bdcf25621a576b10c6
TaehwanGo <gth1123@naver.com>
2021-02-03 05:55:17 +0900

    #19.7 Edit Profile E2E Two

--- cypress/integration/auth/create-account.ts
+++ cypress/integration/auth/create-account.ts
@@ -22,13 +22,7 @@ describe('create Account', () => {
       if (operationName && operationName === 'createAccountMutation') {
         req.reply(res => {
           res.send({
-            data: {
-              createAccount: {
-                ok: true,
-                error: null,
-                __typename: 'CreateAccountOutput',
-              },
-            },
+            fixture: 'auth/create-account.json',
           });
         });
       }
--- cypress/integration/user/edit-profile.ts
+++ cypress/integration/user/edit-profile.ts
@@ -13,8 +13,14 @@ describe('Edit Profile', () => {
     user.title().should('eq', 'Edit Profile | Nuber Eats');
   });
   it('can change email', () => {
+    user.intercept('POST', 'http://localhost:4000/graphql', req => {
+      console.log(req.body);
+      if (req.body?.operationName === 'editProfile') {
+        req.body.variables.input.email = 'testCy@press.com'; // req에 직접 접근해서 아이디가 바뀌지 않도록 해줌
+      }
+    });
     // @ts-ignore
-    // user.login('testCy@press.com', '1234');
+    user.login('testCy@press.com', '1234');
     user.visit('/edit-profile');
     user.findByPlaceholderText(/email/i).clear().type('newtestCy@press.com');
     user.findByRole('button').click();
