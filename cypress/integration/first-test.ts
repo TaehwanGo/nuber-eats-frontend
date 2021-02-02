b13fd831f75c234aad7926898ad1e0facac33a22
TaehwanGo <gth1123@naver.com>
2021-02-03 02:34:19 +0900

    #19.1 Our First Cypress Test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,22 @@
-describe('First test', () => {
-  it('should go to homepage', () => {
-    cy.visit('http://localhost:3000')
-      .title()
-      .should('eq', 'Login | Nuber Eats');
+describe('Log in', () => {
+  it('should see login page', () => {
+    cy.visit('/').title().should('eq', 'Login | Nuber Eats');
+  });
+  it('can fill out the form', () => {
+    cy.visit('/')
+      .get('[name="email"]')
+      .type('asdf@asdf.com')
+      .get('[name="password"]')
+      .type('1234')
+      .findByRole('button')
+      .should('not.have.class', 'pointer-events-none');
+    // todo : can log in
+  });
+  it('can see email / password validation error', () => {
+    cy.visit('/')
+      .findByPlaceholderText(/email/i)
+      .type('bad@email')
+      .findByRole('alert')
+      .should('have.text', 'Please enter a valid email');
   });
 });
